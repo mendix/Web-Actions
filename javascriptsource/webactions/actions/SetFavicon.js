51f3dcd7ed0f715802b7a504d87a852416fba805
Content Teams <widgets@mendix.com>
2022-08-22 10:36:25 +0000

    WC-1274: update actions according to feedback

--- javascriptsource/webactions/actions/SetFavicon.js
+++ javascriptsource/webactions/actions/SetFavicon.js
@@ -16,17 +16,17 @@ function cloneWithNewUrl(link, href) {
 }
 // END EXTRA CODE
 /**
- * @param {string} favicon16x16
- * @param {string} favicon32x32
- * @param {string} appletouchicon
+ * @param {string} iconUrl16x16
+ * @param {string} iconUrl32x32
+ * @param {string} appleTouchIconUrl
  * @returns {Promise.<void>}
  */
-async function SetFavicon(favicon16x16, favicon32x32, appletouchicon) {
+async function SetFavicon(iconUrl16x16, iconUrl32x32, appleTouchIconUrl) {
     // BEGIN USER CODE
     const entries = [
-        ["link[rel=icon][sizes=16x16]", favicon16x16],
-        ["link[rel=icon][sizes=32x32]", favicon32x32],
-        ["link[rel=apple-touch-icon]", appletouchicon]
+        ["link[rel=icon][sizes=16x16]", iconUrl16x16],
+        ["link[rel=icon][sizes=32x32]", iconUrl32x32],
+        ["link[rel=apple-touch-icon]", appleTouchIconUrl]
     ];
     const links = entries.reduce((acc, [selector, href]) => {
         const element = document.querySelector(selector);
